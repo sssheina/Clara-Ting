826c39aa0550c4b017c916cc7e6cefa7f396a8bd
dianamong <saryglardv@gmail.com>
2022-09-27 09:25:59 +0700

    бургер меню

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,18 @@ button.on('click', (e) => {
 }
 
 backToTop();
+
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger, .header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+        
+    });
+});
+
+$(document).ready(function() {
+    $('.header__link').click(function(event) {
+        $('.header__burger, .header__menu').removeClass('active');
+        $('body').removeClass('lock')
+    });
+});
